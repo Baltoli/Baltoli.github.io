d9f375d1d613ea97188675d53dc62c84504dfcfe
Bruce Collie <brucecollie82@gmail.com>
2019-06-10 11:11:19 +0100

    New version

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b77cd3b2767dc236be3a";
+/******/ 	var hotCurrentHash = "24e224882e3cb3be34fa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
