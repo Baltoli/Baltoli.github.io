ca9cb97937f1145e6fbd9651a7cbba1d9ef136a4
Bruce Collie <brucecollie82@gmail.com>
2024-12-16 17:46:43 +0000

    Category update

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "da098e076deb2afacff2";
+/******/ 	var hotCurrentHash = "a8b7bb0779d6024e5074";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
